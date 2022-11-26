f708d54f30202349520904c6c194173d40c66e26
stroeckx <stijnstroeckx@hotmail.com>
2022-11-26 18:58:21 +0100

    Initial commit of v1.0

---
+++ Locales/enUS.lua
@@ -0,0 +1,6 @@
+--localization file for english/United States
+local L = LibStub("AceLocale-3.0"):NewLocale("SGTCore", "enUS", true)
+if not L then return end 
+
+L["Core"] = "Core"
+L["SGTCoreDescription"] = "Stroeckx Goldmaking Toolkit is a modular addon that can provide various utilities, mostly related to making gold in WoW. \nBeing modular allows you to only use the modules you are interested in, while allowing me to create simple addons without having to recreate everything I've made before.\n\nPlease make sure to always update ALL your SGT modules, the curseforge client can update all your addons with one click anyway.\n\n\nIf you have any problems, feedback or suggestions, \nplease join the discord server: https://discord.gg/nYexBdaBuP"
---
+++ init.lua
@@ -0,0 +1,1 @@
+SGTCore = LibStub("AceAddon-3.0"):NewAddon("SGTCore", "AceConsole-3.0", "AceEvent-3.0")
