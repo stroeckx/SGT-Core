dfb8f288d952a5f96d586dc86330b17493816907
Stijn Stroeckx <stijnstroeckx@hotmail.com>
2025-02-21 18:15:15 +0000

    TOC updated for 11.0.7

--- sgtCore.lua
+++ sgtCore.lua
@@ -47,7 +47,7 @@ end
 --Variables start
 SGTCore.majorVersion = 1;
 SGTCore.subVersion = 0;
-SGTCore.minorVersion = 12;
+SGTCore.minorVersion = 13;
 SGTCore.fontStringPools = {};
 SGTCore.backgroundLinePools = {};
 local tabFramesToCreate = {};
